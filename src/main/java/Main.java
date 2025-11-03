4e394c6486006f63b231fa19e088ed89b3231f38
JINX <92461540+0Linxa0@users.noreply.github.com>
2025-11-03 08:29:08 +0000

    3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,22 @@ public class Main {
     Scanner sk =new Scanner(System.in);
     int i = sk.nextInt();
     for(int x=1; x<=i; x++){
+      if(x==1|| x==i){
       for(int y=1; y<=i; y++){
         System.out.print("*");
       }
       System.out.println();
+      }
+      else{
+        for(int y=1; y<=i; y++){
+          if(y==1||y==i){
+          System.out.print("*");
+          }
+          else{
+            System.out.print(" ");
+          }
+        }
+        System.out.println();
+      }
+      
     }}}
