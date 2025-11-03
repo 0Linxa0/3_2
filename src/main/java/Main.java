5a940f7f024d2134dbf9073768d25fced9ea4809
JINX <92461540+0Linxa0@users.noreply.github.com>
2025-11-03 08:31:15 +0000

    3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Podaj rozmiar");
-    Scanner sk =new Scanner(System.in);
+    Scanner sk = new Scanner(System.in);
     int i = sk.nextInt();
-    for(int x=1; x<=i; x++){
-      if(x==1|| x==i){
-      for(int y=1; y<=i; y++){
-        System.out.print("*");
-      }
-      System.out.println();
-      }
-      else{
-        for(int y=1; y<=i; y++){
-          if(y==1||y==i){
+    for (int x = 1; x <= i; x++) {
+      if (x == 1 || x == i) {
+        for (int y = 1; y <= i; y++) {
           System.out.print("*");
-          }
-          else{
+        }
+        System.out.println();
+      } else {
+        for (int y = 1; y <= i; y++) {
+          if (y == 1 || y == i) {
+            System.out.print("*");
+          } else {
             System.out.print(" ");
           }
         }
         System.out.println();
       }
-      
-    }}}
+
+    }
+  }
+}
